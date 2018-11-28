5f7b92717f0d50e2ee16b5ec6d3bf6ee213c80d2
johny <mrbartezi@gmail.com>
2018-11-28 22:14:05 +0100

    Simplified actionPerformed in Computer class

--- src/Computer.java
+++ src/Computer.java
@@ -27,111 +27,41 @@ public class Computer implements ActionListener {
         JButton clicked = (JButton) e.getSource();
         String clickedText = clicked.getText();
 
-        switch (clickedText) {
-            case "0":
-                if(lastOperation != null) {
+        try {
+            if ((Integer.valueOf(clickedText) >= 0 && Integer.valueOf(clickedText) <= 9)) {
+                if (lastOperation != null) {
                     sb.delete(0, sb.length());
                 }
                 sb.append(clickedText);
                 display.addText(sb.toString());
                 temp2 = Integer.valueOf(sb.toString());
-                break;
-            case "1":
-                if(lastOperation != null) {
+            }
+        } catch (Exception exception) {
+
+        } finally {
+            switch (clickedText) {
+                case "C":
+                    display.deleteText();
                     sb.delete(0, sb.length());
-                }
-                if(lastOperation != null) {
-                    sb.delete(0, sb.length());
-                }
-                sb.append(clickedText);
-                display.addText(sb.toString());
-                temp2 = Integer.valueOf(sb.toString());
-                break;
-            case "2":
-                if(lastOperation != null) {
-                    sb.delete(0, sb.length());
-                }
-                sb.append(clickedText);
-                display.addText(sb.toString());
-                temp2 = Integer.valueOf(sb.toString());
-                break;
-            case "3":
-                if(lastOperation != null) {
-                    sb.delete(0, sb.length());
-                }
-                sb.append(clickedText);
-                display.addText(sb.toString());
-                temp2 = Integer.valueOf(sb.toString());
-                break;
-            case "4":
-                if(lastOperation != null) {
-                    sb.delete(0, sb.length());
-                }
-                sb.append(clickedText);
-                display.addText(sb.toString());
-                temp2 = Integer.valueOf(sb.toString());
-                break;
-            case "5":
-                if(lastOperation != null) {
-                    sb.delete(0, sb.length());
-                }
-                sb.append(clickedText);
-                display.addText(sb.toString());
-                temp2 = Integer.valueOf(sb.toString());
-                break;
-            case "6":
-                if(lastOperation != null) {
-                    sb.delete(0, sb.length());
-                }
-                sb.append(clickedText);
-                display.addText(sb.toString());
-                temp2 = Integer.valueOf(sb.toString());
-                break;
-            case "7":
-                if(lastOperation != null) {
-                    sb.delete(0, sb.length());
-                }
-                sb.append(clickedText);
-                display.addText(sb.toString());
-                temp2 = Integer.valueOf(sb.toString());
-                break;
-            case "8":
-                if(lastOperation != null) {
-                    sb.delete(0, sb.length());
-                }
-                sb.append(clickedText);
-                display.addText(sb.toString());
-                temp2 = Integer.valueOf(sb.toString());
-                break;
-            case "9":
-                if(lastOperation != null) {
-                    sb.delete(0, sb.length());
-                }
-                sb.append(clickedText);
-                display.addText(sb.toString());
-                temp2 = Integer.valueOf(sb.toString());
-                break;
-            case "C":
-                display.deleteText();
-                sb.delete(0, sb.length());
-                lastOperation = null;
-                temp1 = temp2 = result = 0;
-                break;
-            case "+":
-                doLastOperation();
-                temp1 = Integer.valueOf(sb.toString());
-                lastOperation = clickedText;
-                temp2 = Integer.valueOf(sb.toString());
-                break;
-            case "=":
-                result = temp1 + temp2;
-                temp1 = result;
-                display.addText(result + "");
-                break;
+                    lastOperation = null;
+                    temp1 = temp2 = result = 0;
+                    break;
+                case "+":
+                    doLastOperation();
+                    temp1 = Integer.valueOf(sb.toString());
+                    lastOperation = clickedText;
+                    temp2 = Integer.valueOf(sb.toString());
+                    break;
+                case "=":
+                    result = temp1 + temp2;
+                    temp1 = result;
+                    display.addText(result + "");
+                    break;
+            }
         }
     }
     public void doLastOperation() {
-        if (lastOperation != null) {
+        if (lastOperation != null && lastOperation != "") {
             if (lastOperation.equals("+")) {
                 temp1 += temp2;
             }
